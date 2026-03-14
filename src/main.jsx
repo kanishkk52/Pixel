58323fb18c9babb28666b9f64559fb7b0fc69057
Say My Name! <kanishkk52@gmail.com>
2026-03-15 01:53:52 +0530

    google console

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import App from "./App"
 import "./index.css"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="578954072006-aas3viqv21ho7hc2gvg64ibpac1ljsmr.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 )
